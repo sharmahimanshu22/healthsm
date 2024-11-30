4bd29b3af603f27b8eb73ae3aea22532d1810dcb
Himanshu Sharma <himanshusharma.sh7@gmail.com>
2024-11-30 12:21:12 -0500

    Second Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,29 @@
 import Image from "next/image";
+import Hero from "../components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { FaHome } from "react-icons/fa";
+import Grid from "@/components/Grid";
 
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 FLEX justify-center items-center flex-col overflow-hidden 
+    mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={[
+          {name:'Home', link: '/', icon:<FaHome /> }
+        ]}/>
+       <Hero />
+       <Grid />
+      </div>
+    </main>
+
+  )
+}
+
+
+
+/*
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -99,3 +123,5 @@ export default function Home() {
     </div>
   );
 }
+
+*/
