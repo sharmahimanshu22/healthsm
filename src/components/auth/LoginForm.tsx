0c620a7f833530a5803dc138f79c3e359e8b8ac4
Himanshu Sharma <himanshusharma.sh7@gmail.com>
2024-12-10 20:58:28 -0500

    Change in text

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -19,12 +19,8 @@ export function LoginForm() {
   return (
     <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-        Welcome to Aceternity
+        Welcome to Matrix
       </h2>
-      <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-        Welcome to the matrix
-        yet
-      </p>
 
       <form className="my-8" onSubmit={handleSubmit}>
         <LabelInputContainer className="mb-4">
