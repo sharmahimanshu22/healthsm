67aac96d98ea0ec720e02da70f4aef4db7cd8e3c
Himanshu Sharma <himanshusharma.sh7@gmail.com>
2024-12-10 20:18:46 -0500

    Modified Login form from default

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -85,16 +85,7 @@ export function LoginForm() {
             </span>
             <BottomGradient />
           </button>
-          <button
-            className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-            type="submit"
-          >
-            <IconBrandOnlyfans className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-            <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-              OnlyFans
-            </span>
-            <BottomGradient />
-          </button>
+          
         </div>
       </form>
     </div>
@@ -123,3 +114,4 @@ const LabelInputContainer = ({
     </div>
   );
 };
+
